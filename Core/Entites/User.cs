85b815ae39547a80f16082117cbc6c0584b571d6
Rostislav Fedoriv <froizerner@gmail.com>
2022-10-18 16:59:57 +0300

    Added user enity, added role entity

---
+++ Core/Entites/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Entities;
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string SecondName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public byte[]? PasswordHash { get; set; }
+    public byte[]? PasswordSalt { get; set; }
+
+    public int RoleId { get; set; }
+    public Role? Role { get; set; }
+}
