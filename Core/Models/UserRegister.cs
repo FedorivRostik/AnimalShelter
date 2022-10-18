99ed8c62f14f071810bc0489dec53580325e8df8
Rostislav Fedoriv <froizerner@gmail.com>
2022-10-18 17:04:45 +0300

    Added models for login and register

---
+++ Core/Models/UserRegister.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Models;
+public class UserRegister
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string SecondName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
